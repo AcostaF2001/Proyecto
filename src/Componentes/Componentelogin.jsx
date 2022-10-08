b5f95b655ad22e11ca90a607386983b52b1a2b57
AcostaF2001 <98819485+AcostaF2001@users.noreply.github.com>
2022-10-08 01:31:52 -0500

    Google
    afaf

--- src/Componentes/Componentelogin.jsx
+++ src/Componentes/Componentelogin.jsx
@@ -1,21 +1,27 @@
 import { Logingoogle } from "./logingoogle"
-import { Loginnumero } from "./loginnumero"
+import { Logincorreo } from "./logincorreo"
 import { Componenteayuda } from "./Componenteayuda"
 import { useState } from "react"
 import Logo from "./logo.png"
 import GoogleButton from "react-google-button"
 import { UserAuth } from "./Firebase/GoogleAuth"
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 export const Componentlogin=()=>{
 
-    const {googleSignIn} =UserAuth();
-
+    const {googleSignIn,user} =UserAuth();
+    const navigate= useNavigate();
     const HandleGoogleSignIn= async()=>{
         try{
             await googleSignIn();
-
+            navigate("./home")
         }catch(error){
             console.log(error)
         }
+    }
+
+    const RegistrarseCorreo=()=>{
+        navigate("./Registro")
     }
 
 
@@ -28,10 +34,13 @@ export const Componentlogin=()=>{
          <div>
             <GoogleButton onClick={HandleGoogleSignIn}/>
          </div>
-         
-         
+
+         <div>
+            <button className="btn btn-primary" onClick={RegistrarseCorreo}>Correo y Contraseña</button>
+         </div>
          
 
         </>
     )
+
 }
--- src/Componentes/Firebase/GoogleAuth.js
+++ src/Componentes/Firebase/GoogleAuth.js
@@ -1,4 +1,4 @@
-import { useContext,createContext } from "react";
+import { useContext,createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider,signInWithRedirect,signOut,onAuthStateChanged} from "firebase/auth";
 import { auth } from "./Config";
 
@@ -6,13 +6,28 @@ const AuthContext= createContext()
 
 export const AuthContextProvider=({children}) =>{
 
+    const [user,setUser]=useState({});
+
     const googleSignIn=() =>{
         const provider = new GoogleAuthProvider();
         signInWithRedirect(auth,provider)
     }
+
+    const logOut =()=>{
+        signOut(auth)
+    }
+    useEffect(()=>{
+        const unsuscribe=onAuthStateChanged(auth,(currentUser)=>{
+            setUser(currentUser);
+            console.log("user",currentUser)
+        })
+        return ()=>{
+            unsuscribe();
+        }
+    },[])
     
     return(
-        <AuthContext.Provider value={{googleSignIn}}>
+        <AuthContext.Provider value={{googleSignIn,logOut,user}}>
             {children}
         </AuthContext.Provider>
 
